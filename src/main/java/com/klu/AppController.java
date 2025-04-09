652c45db61e30f6362bdcef0024803747a81f304
kodavali.lakshmi@gmail.com <kodavali.lakshmi@kluniversity.in>
2025-04-09 10:08:56 +0530

    second commit

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +21,11 @@ public class AppController {
     {
     	return s.retrieveData();
     }
+    @GetMapping("/message")
+	public String msg() {
+		String univ = "KLEF";
+		return " I study at " + univ;
+	}
   
 
 }
